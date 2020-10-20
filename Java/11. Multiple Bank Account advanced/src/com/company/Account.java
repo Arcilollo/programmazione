714b791bdbbdd0b4f3a06f453e91672e5254b50e
Arcilollo <arcilollo@tutanota.com>
2020-10-20 19:17:31 +0200

    Added more functions in the 11th Java program
    now the program calculate the montly interest and the annual costs of the account

--- Java/11. Multiple Bank Account advanced/src/com/company/Account.java
+++ Java/11. Multiple Bank Account advanced/src/com/company/Account.java
@@ -4,19 +4,29 @@ public class Account {
 	private int account_number;
 	private String man;
 	private int money;
+	private int isPayed;
+	private int month;
+	private int interestsPercent;
 
 	public Account (int a, String b, int c) {
 		this.account_number = a;
 		this.man = b;
 		this.money = c;
+		this.isPayed = 0;
+		this.month = 1;
+		this.interestsPercent = 3;
 	}
 
 	public int getMoney() {
 		return this.money;
 	}
 
-	public int getAccount_number() {
-		return account_number;
+	public int getMonth() {
+		return this.month;
+	}
+
+	public int getIsPayed() {
+		return isPayed;
 	}
 
 	public void addMoney(int a) {
@@ -26,4 +36,45 @@ public class Account {
 	public void removeMoney(int a) {
 		this.money -= a;
 	}
+
+	public String payAnnualCosts(int a) {
+		String pay_msg;
+		if ((this.getMoney() > a) && (this.isPayed == 0)) {
+			this.removeMoney(a);
+			this.isPayed = 1;
+			pay_msg = "Hai pagato " + a + " euro per la gestione annua del tuo conto.";
+		}
+		else
+			pay_msg = "Non hai abbastanza soldi per pagare i costi di gestione annua del tuo conto";
+
+
+		return pay_msg;
+	}
+
+	private void interests(int a)
+		this.money = this.money + (this.money / 100 * a);
+	}
+
+	public String nextMonth() {
+		String msg = " ";
+		if (this.month == 12) {
+			this.month = 1;
+			if (this.isPayed == 0) {
+				this.removeMoney(60);
+				msg = "Le spese di gestione sono state pagate automaticamente.\n";
+			}
+			else
+				this.isPayed = 0;
+
+		}
+		else
+			this.month++;
+
+		this.interests(this.interestsPercent);
+		msg += "Hai ricevuto " + (this.money / 100 * this.interestsPercent) + " euro di interessi.";
+
+		return msg;
+
+	}
+
 }
--- Java/11. Multiple Bank Account advanced/src/com/company/Main.java
+++ Java/11. Multiple Bank Account advanced/src/com/company/Main.java
@@ -19,23 +19,37 @@ public class Main {
 
 		Account account = new Account(account_number, man, money);
 
-		int r;
+		int r, input;
+		String [] monthName = { "Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre" };
 		do {
+			System.out.println("il mese di oggi è: " + monthName [account.getMonth() - 1]);
 			System.out.println("1. Deposita");
 			System.out.println("2. Preleva");
-			System.out.println("3, Visualizza saldo");
+			System.out.println("3. Visualizza saldo");
+			System.out.println("4. Prossimo mese");
+			if ((account.getIsPayed() == 0) && (account.getMonth() >= 11))
+				System.out.println("5. Paga i costi di gestione annuo");
 			System.out.println("0. Esci");
 			r = scan.nextInt();
 
 			switch (r) {
 				case 1:
-					account.addMoney(scan.nextInt());
+					input = scan.nextInt();
+					account.addMoney(input);
 					break;
 				case 2:
-					account.removeMoney(scan.nextInt());
+					input = scan.nextInt();
+					account.removeMoney(input);
 					break;
 				case 3:
 					System.out.println("Il tuo saldo è: " + account.getMoney());
+					break;
+				case 4:
+					System.out.println(account.nextMonth());
+					break;
+				case 5:
+					input = 60;
+					System.out.println(account.payAnnualCosts(input));
 					break;
 			}
 		} while (r != 0);
