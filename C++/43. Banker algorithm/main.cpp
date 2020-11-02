22c6a45bea0563c23d0d1c6440fe679a307bca91
Arcilollo <arcilollo@tutanota.com>
2020-11-02 20:44:42 +0100

    updated C++ program

--- C++/43. Banker algorithm/main.cpp
+++ C++/43. Banker algorithm/main.cpp
@@ -2,9 +2,17 @@
 
 using namespace std;
 
+void assegnazioni(int proc[], int nProc, int &freeRes) {
+	for (int i = 0; i < nProc; i++) {
+		cout << "Inserisci risorse assegnate a P" << i+1 << ": ";
+		cin  >> proc[i];
+		freeRes -= proc[i];
+	}
+}
+
 void richieste(int proc[], int nProc) {
 	for (int i = 0; i < nProc; i++) {
-		cout << "Inserisci Richieste da parte di P" << i+1 << ": ";
+		cout << "Inserisci richieste rimanenti da parte di P" << i+1 << ": ";
 		cin  >> proc[i];
 	}
 }
@@ -17,11 +25,15 @@ int main() {
 	cin  >> nProc;
 	cout << "Inserisci la quantità di instanze di R1: ";
 	cin  >> maxRes;
+
 	freeRes = maxRes;
+	int Aproc[nProc];
+	int Rproc[nProc];
 
-	int proc[nProc];
+	assegnazioni(Aproc, nProc, freeRes);
+	richieste(Rproc, nProc);
 
-	richieste(proc, nProc);
+	cout << freeRes << " / " << maxRes << "\n";
 
 	return 0;
 }
