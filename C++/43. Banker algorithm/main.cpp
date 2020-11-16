056de8a0714edab70d269fc7b842f64876347bb7
Arcilollo <arcilollo@tutanota.com>
2020-11-16 08:29:34 +0100

    the Banker algorithm still doesn't work
    Anyways I commited it

--- C++/43. Banker algorithm/main.cpp
+++ C++/43. Banker algorithm/main.cpp
@@ -2,18 +2,28 @@
 
 using namespace std;
 
-void zeroM(int M[][100], int n, int m) {	// Funzione per azzerare una matrice
+void printM(int max[][100], int n, int m) {
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			M[i][j] = 0;
+			cout << max[i][j] << " ";
 		}
+		cout << "\n";
 	}
 }
 
 void setFree(int free[], int m) {	// Inserimento del numero di instanze
 	for (int i = 0; i < m; i++) {
-		cout << "Inserisci quante instanze ci sono in R" << i+1 << ": ";
+		cout << "Inserisci quante instanze sono disponibili in R" << i+1 << ": ";
 		cin  >> free[i];
+	}
+}
+
+void setAllocated(int max[][100], int n, int m) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << "Inserisci quante risorse sono assegnate a P" << i+1 << " da R" << j+1 << ": ";
+			cin  >> max[i][j];
+		}
 	}
 }
 
@@ -23,6 +33,20 @@ void setMax(int max[][100], int n, int m) {
 			cout << "Inserisci quante risorse chiede P" << i+1 << " ad R" << j+1 << ": ";
 			cin  >> max[i][j];
 		}
+	}
+}
+
+void calculateNec(int nec[][100], int n, int m, int max[][100], int allocated[][100]) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			nec[i][j] = max[i][j] - allocated[i][j];
+		}
+	}
+}
+
+void printSafe(int safe[], int n) {
+	for (int i = 0; i < n; i++) {
+		cout << safe[i] << "\n";
 	}
 }
 
@@ -38,13 +62,49 @@ int main() {
 	int free[m];			// Instanze disponibili
 	int max[n][100];		// Numero massimo di instanze
 	int allocated[n][100];		// Instanze assegnate
-	int nec[n][100];		// max[][] - allocated[]
+	int nec[n][100];		// max[][] - allocated[][]
 
 	setFree(free, m);
 	setMax(max, n, m);
-	zeroM(allocated, n, m);
-	zeroM(nec, n, m);
+	setAllocated(allocated, n, m);
 
-	int safe[n];
+	int safeSeq[n], safeindex = 0; 	// Array che memorizza la sequenza sicura
+	bool safe;			// variabile che serve per vedere se il processo può entrare nella sequenza sicura o no
 
+	for (int i = 0; safeindex < n; i++) {
+		safe = true;
+		for (int j = 0; j < m; j++) {
+			int zeroCount = 0;
+			if (nec[i][j] > free[j]) {
+				safe = false;
+				cout << nec[i][j] << " > " << free[j];
+			}
+
+			if (nec[i][j] == 0)
+				zeroCount++;
+			if (zeroCount == m)
+				safe = false;
+		}
+		if (safe == true) {
+			for (int j = 0; j < m; j++) {
+				allocated[i][j] += nec[i][j];
+				free[j] -= nec[i][j];
+				nec[i][j] = 0;
+			}
+			for (int j = 0; j < m; j++) {
+				free[j] += allocated[i][j];
+				allocated[i][j] = 0;
+			}
+			safeSeq[safeindex] = i;
+			safeindex++;
+		}
+		if (i == n - 1)
+			i = 0;
+		cout << safe << "\n";
+	}
+
+	cout << "la sequenza sicura è: \n";
+	printSafe(safeSeq, n);
+
+	return 0;
 }
