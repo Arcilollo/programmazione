a4f882488fc051047506b7f728fff56a7441d658
Lorenzo Arcidiacono <arcilollo@tutanota.com>
2021-03-16 22:21:22 +0100

    Fixed things

--- C/11. multiple fork/main.c
+++ C/11. multiple fork/main.c
@@ -18,14 +18,19 @@ int main() {
 
 	if (pid1 == 0) {
 		printf("PID FIGLIO: %d\nPID DEL PADRE: %d\n\n", getpid(), getppid());
+		sleep(1);
 		exit(10);
 	}
 	else if (pid1 > 0) {
+		printf("Processo PADRE\nin attesa che il primo processo figlio termini\n");
+		pid_wait = wait(&status);
+
+		/* Ultimo processo terminato
 		for (int i = 0; i < nForks; i++)
 			pid_wait = wait(&status);
-		printf("Processo PADRE\n");
-		printf("pid figlio1 terminato: %d\n", pid_wait);
-		//printf("Valore ritornato dal figlio: %d\n", status/256);
+		*/
+		printf("pid del processo figlio terminato: %d\n", pid_wait);
+		printf("valore restituito dal processo figlio terminato: %d\n", status/256);
 	}
 	else
 		printf("errore nella fork\n");
